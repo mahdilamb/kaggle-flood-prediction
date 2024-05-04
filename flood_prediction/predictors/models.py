45ce120462adcc1c7650a1b371b4e95a5cb0e4c4
Mahdi Lamb <mahdilamb@gmail.com>
2024-05-04 09:20:23 +0100

    lint + detail in semver-checker

--- flood_prediction/predictors/models.py
+++ flood_prediction/predictors/models.py
@@ -1,13 +1,1 @@
-import polars as pl
-
-from flood_prediction import constants, dataset
-
-
-def one_liner():
-    """One liner taken from https://www.kaggle.com/competitions/playground-series-s4e5/discussion/499263."""
-    return dataset.load("test").select(
-        "id",
-        ((pl.sum_horizontal(pl.exclude("id")) * 0.0056) - 0.05).alias(
-            constants.OUTPUT_FEATURE
-        ),
-    )
+"""Module containing the models used for regression."""
